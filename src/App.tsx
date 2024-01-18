4e65c97a94b556a12ab431f409a52fc7b8cda798
Arian Tershnjaku <ariantershnjaku@gmail.com>
2024-01-18 23:53:02 +0100

    Currency converter

--- src/App.tsx
+++ src/App.tsx
@@ -161,3 +161,63 @@ export default function App() {
     </>
   );
 }
+
+//CHALLENGE 1: CURRENCY CONVERTER
+// import { useEffect, useState } from "react";
+
+// type Rates = {
+//   amount: number;
+//   base: string;
+//   date: string;
+//   rates: {
+//     [key: string]: number;
+//   };
+// };
+
+// export default function App() {
+//   const [amountRes, setAmount] = useState(1);
+//   const [currencyFrom, setCurrencyFrom] = useState("EUR");
+//   const [currencyTo, setCurrencyTo] = useState("USD");
+//   const [converted, setConverted] = useState("");
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       const res = await fetch(
+//         `https://api.frankfurter.app/latest?amount=${amountRes}&from=${currencyFrom}&to=${currencyTo}`
+//       );
+//       const data = await res.json();
+//       setConverted(data.rates[currencyTo]);
+//     }
+//     fetchData();
+//   }, [amountRes, currencyFrom, currencyTo]);
+//   return (
+//     <div>
+//       <input
+//         defaultValue={amountRes}
+//         type="text"
+//         onChange={(e) => setAmount(Number(e.target.value))}
+//       />
+//       <select
+//         value={currencyFrom}
+//         onChange={(e) => setCurrencyFrom(e.target.value)}
+//       >
+//         <option value="USD">USD</option>
+//         <option value="EUR">EUR</option>
+//         <option value="CAD">CAD</option>
+//         <option value="INR">INR</option>
+//       </select>
+//       <select
+//         value={currencyTo}
+//         onChange={(e) => setCurrencyTo(e.target.value)}
+//       >
+//         <option value="USD">USD</option>
+//         <option value="EUR">EUR</option>
+//         <option value="CAD">CAD</option>
+//         <option value="INR">INR</option>
+//       </select>
+//       <p>
+//         {converted} {currencyTo}
+//       </p>
+//     </div>
+//   );
+// }
